d862850db3d83a9437280de3827b25e654cb45e1
SpikeHD <spikegdofficial@gmail.com>
2023-12-14 15:10:56 -0800

    fix: prevent rapid dom update

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,43 @@
+import { getPlaylistId, getRadioName } from './util/meta'
 import { getCurrentSong } from './util/radio'
 import { playlistVideos } from './util/youtube'
 
-/**
- * We will have two URL params, the "name" and the "id". Name is used for embedding, ID is used for getting the playlist.
- * 
- * The name will not matter, as its just for people reading the URL to identify the playlist (especially for embeding in sites and stuff).
- * The playlist ID does matter. Obviously.
- */
-const playlistId = new URLSearchParams(window.location.search).get('id')!
-const name = new URLSearchParams(window.location.search).get('name')!
+// Change the meta tags to reflect the playlist, if there is one
+if (getPlaylistId() && getRadioName()) {
+  const name = getRadioName()
+  const titles = document.querySelectorAll('meta[name*="title"]')
+  const descriptions = document.querySelectorAll('meta[name*="description"]')
+
+  for (const title of titles) {
+    title.setAttribute('content', 'Titune')
+  }
+
+  for (const description of descriptions) {
+    description.setAttribute('content', `Listen along to "${name}" Radio`)
+  }
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   // Every second, update currently-playing and time-elapsed
   const currentlyPlaying = document.getElementById('currently-playing')!
   const timeElapsed = document.getElementById('time-elapsed')!
+  const playlistId = getPlaylistId()
+  let identifier = ''
 
   // Ensure we call this at least once, to cache the playlist
   await playlistVideos(playlistId)
 
   setInterval(async () => {
     const { song, elapsed } = await getCurrentSong(playlistId)
+
+    // Prevents rapid DOM updates
+    if (`${song?.title ?? 'Unknown'}${elapsed}` === identifier) {
+      return
+    }
+
     currentlyPlaying.textContent = song?.title ?? 'Unknown'
     timeElapsed.textContent = `${Math.floor(elapsed / 60)}:${Math.floor(elapsed % 60).toString().padStart(2, '0')}`
-  }, 100)
+
+    identifier = `${song?.title ?? 'Unknown'}${elapsed}`
+  }, 200)
 })
---
+++ src/util/meta.ts
@@ -0,0 +1,13 @@
+/**
+ * We will have two URL params, the "name" and the "id". Name is used for embedding, ID is used for getting the playlist.
+ * 
+ * The name will not matter, as its just for people reading the URL to identify the playlist (especially for embeding in sites and stuff).
+ * The playlist ID does matter. Obviously.
+ */
+export function getRadioName() {
+  return new URLSearchParams(window.location.search).get('name')!
+}
+
+export function getPlaylistId() {
+  return new URLSearchParams(window.location.search).get('id')!
+}
