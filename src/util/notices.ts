19380c46a5489d2fa87f108483ae5135c590cc73
SpikeHD <spikegdofficial@gmail.com>
2023-12-21 19:28:19 -0800

    feat: semi useful error messages

---
+++ src/util/notices.ts
@@ -0,0 +1,28 @@
+const notices: HTMLDivElement[] = []
+
+export async function createNotice(message: string, type: string) {
+  const notice = document.createElement('div')
+  const container = document.querySelector('#notice-container')!
+  notice.classList.add('notice')
+  notice.classList.add(type)
+  notice.textContent = message
+
+  const inst = container.appendChild(notice)
+  
+  // Wait to ensure its mounted in the DOM
+  await new Promise(resolve => setTimeout(resolve, 100))
+
+  // Wow fade in anim!!! what!!!
+  inst.classList.add('show')
+
+  notices.push(inst)
+
+  // Remove the notice after 5 seconds
+  setTimeout(() => {
+    inst.classList.remove('show')
+    setTimeout(() => {
+      inst.remove()
+      notices.splice(notices.indexOf(inst), 1)
+    }, 100)
+  }, 5000)
+}
